d9102591ff2d413e5e9bf54e79bf32c1d62ceacd
Adithya Vel M <adithyamuralikrishnan512@gmail.com>
2026-08-09 13:38:37 +0530

    feat: add delete URL route with protection middleware

--- url-shortener/src/routes/urlRoutes.js
+++ url-shortener/src/routes/urlRoutes.js
@@ -1,15 +1,22 @@
 import express from "express";
+
 import {
   createShortUrl,
   redirectToOriginalUrl,
   getAllUrls,
+  deleteUrl,
 } from "../controllers/urlController.js";
+
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/shorten", protect, createShortUrl);
+
 router.get("/all", protect, getAllUrls);
+
+router.delete("/:id", protect, deleteUrl);
+
 router.get("/:shortCode", redirectToOriginalUrl);
 
 export default router;
